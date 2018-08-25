42e10ca8e0de51e6e2e262bc8b1292d6ac373432
Mesi Rendon <amrendonsa@unal.edu.co>
2018-08-25 10:52:53 -0500

    More tests

--- contracts/Certifier.sol
+++ contracts/Certifier.sol
@@ -1,7 +1,28 @@
 pragma solidity ^0.4.23;
 
 contract Certifier {
-    uint8 max_st = 8;
+    struct Course {
+        string name;
+        uint8 students;
+        uint start;
+        uint end;
+        uint8 duration;
+        uint8 threshold;
+        bytes32[] session_codes;
+        uint8 cost;
+    }
+
+    address public certifierAccount;
+    mapping(bytes32 => Course) courses;
+
+    constructor () public {
+        certifierAccount = msg.sender;
+    }
+
+    modifier onlyCertifier () {
+        require(msg.sender == certifierAccount);
+        _;
+    }
 
     function addCourse(
         string _name,
@@ -12,7 +33,16 @@ contract Certifier {
         uint8 _hours,
         uint8 _threshold,
         bytes32[] _session_codes,
-        uint8 _cost) public view returns(uint8 mxst) {
-        mxst = max_st;
+        uint8 _cost) public onlyCertifier {
+        courses[_code] = Course({
+            name : _name,
+            students : _max_st,
+            start : _start,
+            end : _end,
+            duration : _hours,
+            threshold : _threshold,
+            session_codes : _session_codes,
+            cost : _cost
+            });
     }
 }
--- test/01-certifier.js
+++ test/01-certifier.js
@@ -1,27 +1,43 @@
-const Certifier = artifacts.require('./Certifier');
+const Certifier = artifacts.require('./Certifier')
 
-chai = require('chai');
+chai = require('chai')
 
-expect = chai.expect;
+expect = chai.expect
+
+const willBeRejected = promise => {
+  promise
+    .then(result => {expect(result).to.be.an('error')})
+    .catch(error => {expect(error).to.be.an('error')})
+}
+
+const willBeFulfilled = promise => {
+  promise
+    .then(result => {expect(result).not.be.an('error')})
+    .catch(error => {expect(error).not.be.an('error')})
+}
 
 contract('Certifier', accounts => {
   describe('Instance', () => {
     it('should get an instance of Certifier', () => {
       Certifier.deployed().then(instance => {
-        certifier = instance;
-        expect(certifier).not.to.be.an('error');
+        certifier = instance
+        expect(certifier).not.to.be.an('error')
       })
-    });
-  });
+    })
+  })
   describe('Flow', () => {
     it('should allow the certifier to create the course', () => {
-      certifier.addCourse('Solidity', 'SE', 30, 1, 5, 45, 80, ['a', 'b', 'c', 'xyz', 'abc'], 3)
-        .then(response => {
-          expect(reponse.tx).to.match(/\w{66}/)
-        })
-    });
-  });
-  describe('Finishing', () => {
-    it('should assert true is true');
+      willBeFulfilled(certifier.addCourse('Solidity', 'SE', 30, 1, 5, 45, 80, ['a', 'b', 'c', 'xyz', 'abc'], 3))
+    })
+    it('should avoid other accounts to create courses', () => {
+      willBeRejected(certifier.addCourse('Solidity', 'SE', 30, 1, 5, 45, 80, ['a', 'b', 'c', 'xyz', 'abc'], 3, {from: accounts[1]}))
+    })
+    it('should allow a student to subscribe to a given course', () => {
+      // willBeFulfilled(certifier.subscribe('SE', 'Pepito', 'Perez', 'CC123', 10, 'p@per.ez', {from: accounts[2], value: 2700000000000000000}))
+    })
+    it('should assert true is true', () => {expect(true).to.be.true})
   })
-});
+  describe('Finishing', () => {
+    it('should assert true is true', () => {expect(true).to.be.true})
+  })
+})
